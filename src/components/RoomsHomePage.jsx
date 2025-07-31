5fe1d58c2a55c6a78b252ea44709453980c2e66b
Ravindra Kamble <write2ravi14@gmail.com>
2025-07-31 18:27:09 +0530

    dsdsd

--- src/components/RoomsHomePage.jsx
+++ src/components/RoomsHomePage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { UserRound, IndianRupee } from "lucide-react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SectionTitle from "./SectionTitle.jsx";
-import {rooms} from '../data/rooms.js';
+import { rooms } from "../data/rooms.js";
 import "swiper/css";
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
 function RoomsHomePage() {
-  const swiperBreaks= {
+  const swiperBreaks = {
     640: {
       slidesPerView: 2,
     },
@@ -40,30 +42,44 @@ function RoomsHomePage() {
       </div>
       <div className="rooms-slider-container">
         <Swiper
-  modules={[Navigation, Pagination, Scrollbar, A11y]}
-  spaceBetween={20}
-  slidesPerView={1}
-  navigation={true}
-  pagination={{ clickable: true }}
-  scrollbar={{ draggable: true }}
-  className="rooms-swiper-container"
-  breakpoints={swiperBreaks}
->
-  {rooms.map((room) => (
-    <SwiperSlide
-      key={room.id}
-      className="swiper-slide"
-      style={{
-        backgroundImage: `url(${room.bannerImg})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="room-slider-inside"></div>
-    </SwiperSlide>
-  ))}
-</Swiper>
-
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={20}
+          slidesPerView={1}
+          navigation={true}
+          pagination={{ clickable: true }}
+          scrollbar={{ draggable: true }}
+          className="rooms-swiper-container"
+          breakpoints={swiperBreaks}
+        >
+          {rooms.map((room) => (
+            <SwiperSlide
+              key={room.id}
+              className="room-swiper-slide"
+              style={{
+                backgroundImage: `url(${room.bannerImg})`,
+              }}
+            >
+              <div className="room-slider-inside">
+                <h3>{room.name}</h3>
+                <h4>{room.location}</h4>
+                <div className="room-guest-info">
+                  <p>
+                    <span>35</span>Feets Size
+                  </p>
+                  <p>
+                    <UserRound /> 2 Guests
+                  </p>
+                </div>
+                <p className="room-price">
+                  <span>₹</span> {room.price}
+                </p>
+                <button className="room-swiper-book-btn">
+                  <Link to = {`/rooms/${room.id}`} state={room}>Book Now</Link>
+                </button>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
--- src/pages/RoomDetails.jsx
+++ src/pages/RoomDetails.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
 
 function RoomDetails() {
+  const location = useLocation();
+  const room = location.state;
+  console.log(room);
   return (
-    <div>RoomDetails</div>
+    <div className="room-details-banner">
+      <h2></h2>
+    </div>
   )
 }
 
