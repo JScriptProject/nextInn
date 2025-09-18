9f6e0dcb2ee96114cfd12b840544e58168161eb0
Ravindra <write2ravi14@gmail.com>
2025-09-18 09:58:24 +0530

    from laptop

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,14 @@
-import express from 'express';
-import app from './app.js';
-import path from 'path';
+import express from "express";
+import app from "./app.js";
+import path from "path";
 import { fileURLToPath } from "url";
-import {connectDB} from './db/index.js';
-import coresMiddleware from './midlewares/cors.middleware.js';
-import loggerMiddleware from './midlewares/logger.midleware.js'; 
-import { errorhandler } from './midlewares/errorsHandler.middleware.js';
-
-import roomsRouter from './routers/roomsRouter.js';
+import { connectDB } from "./db/index.js";
+import coresMiddleware from "./midlewares/cors.middleware.js";
+import loggerMiddleware from "./midlewares/logger.midleware.js";
+import { errorhandler } from "./midlewares/errorsHandler.middleware.js";
+import roomsRouter from "./routers/roomsRouter.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
@@ -21,27 +22,25 @@ app.use(coresMiddleware);
 app.use(loggerMiddleware);
 
 //routers
-app.use('/api', roomsRouter);
+app.use("/api", roomsRouter);
 
-// create static files for images 
+// create static files for images
 
 const filePath = path.dirname(fileURLToPath(import.meta.url));
 console.log(filePath);
-const mediaPath = path.join(filePath,"assets","media");
-app.use("/media", express.static(mediaPath))
+const mediaPath = path.join(filePath, "assets", "media");
+app.use("/media", express.static(mediaPath));
 
-connectDB()
-.then(()=>{
-    app.listen(PORT, ()=>{
-        console.log(`NextInn backend Server started on Port ${PORT}`);
-    })
-    app.on("error", (err)=>{
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`NextInn backend Server started on Port ${PORT}`);
+  });
+  app.on("error", (err) => {
     console.log(`Error occured: ${err.message}`);
-})
-})
+  });
+});
 
 app.use(errorhandler);
-
 
 // app.listen(PORT, ()=>{
 //     console.log(`NextInn backend Server started on Port ${PORT}`);
