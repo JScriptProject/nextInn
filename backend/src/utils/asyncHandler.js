a4aa4cd318dd5c49f81298b8e28a2aae0fb9fc53
Ravindra Kamble <write2ravi14@gmail.com>
2025-10-01 16:25:04 +0530

    new folder created as frontend

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,6 +1,8 @@
-const asyncHandler = (requestHandler) =>{
-    return (req,res,next)=>{
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+const asyncHandler = (controllerFun) =>{
+
+    return(req,res,next)=>{
+        Promise.resolve(controllerFun(req,res,next)).catch((err)=>next(err))
     }
 }
-export {asyncHandler};
+
+export  {asyncHandler}
