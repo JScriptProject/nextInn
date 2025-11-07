231b72036714ca31cb82c7ff60b5a5e136f12716
Ravindra Kamble <write2ravi14@gmail.com>
2025-11-07 20:29:14 +0530

    push

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { signup } from '../api/athenticationApi.js';
+import { signup } from '../api/authenticationApi.js';
 function Register() {
   const [form, setForm] = useState({
     firstname: "",
