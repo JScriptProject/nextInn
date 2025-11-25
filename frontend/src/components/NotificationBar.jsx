61de121b3b043ff3bb73c21e25a3e13870b35fe6
Ravindra Kamble <write2ravi14@gmail.com>
2025-11-25 13:36:24 +0530

    notification fixed

--- frontend/src/components/NotificationBar.jsx
+++ frontend/src/components/NotificationBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createPortal } from 'react-dom'
 import { useContext } from 'react';
-import { NotificationContext } from '../context/NotificationContext';
+import { NotificationContext } from '../context/notificationContext';
 
 function NotificationBar() {
   const { notification } = useContext(NotificationContext);
