1e427b21f9d8912b47f1cd983695f93322b600a2
Ravindra Kamble <write2ravi14@gmail.com>
2025-11-26 12:16:36 +0530

    commit from laptop

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import "@assets/fonts/fonts.css";
 import "@assets/css/index.css";
 
-import App from "@utils/App.jsx";
-import SmoothScrollProvider from "@utils/SmoothScrollWrapper.jsx";
+import App from "./App.jsx";
+import SmoothScrollProvider from "./SmoothScrollWrapper.jsx";
 
 import { Provider } from "react-redux";
 import store from "@redux/store.js";
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@api": path.resolve(__dirname, "src/api"),
+      "@redux": path.resolve(__dirname, "src/redux"),
+      "@utils": path.resolve(__dirname, "src/util"),
+      "@user": path.resolve(__dirname, "src/userUI"),
+      "@admin": path.resolve(__dirname, "src/adminUI"),
+    },
+  },
+});
