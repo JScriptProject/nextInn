c5f07aeee563aaa4eb1a4bfe72c66c1fe812ffc1
Ravindra Kamble <write2ravi14@gmail.com>
2026-01-26 16:48:41 +0530

    Admin logout fixed

--- backend/src/controllers/authControllers/adminLogout.controller.js
+++ backend/src/controllers/authControllers/adminLogout.controller.js
@@ -1,9 +1,9 @@
 import { asyncHandler } from "../../utils/asyncHandler.js";
 
+const adminLogout = asyncHandler(async (req, res, next) => {
+    const body = req.body || {};
+    console.log("Logged out succesfully!!");
+    res.success(200, "Logged out succesfully");
+});
 
-const adminLogout = asyncHandler((req, res, next)=>{
-
-    
-})
-
-export {adminLogout}
+export { adminLogout };
